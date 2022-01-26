5928e3be1b14d805a3519fd5920240f99d9f549b
Ernestekas <ernestekas@gmail.com>
2022-01-26 22:26:00 +0200

    Fetching data from my web api.

--- ShopApp/Shop-App/src/app/app.component.ts
+++ ShopApp/Shop-App/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Shop-App';
+  private data: any = [];
+
+  constructor(private http: HttpClient){
+
+  }
+
+  getData(){
+    const url = 'https://localhost:44300/api/Products'
+    this.http.get(url).subscribe((res)=>{
+      this.data = res
+      console.log(this.data)
+    })
+  }
 }
