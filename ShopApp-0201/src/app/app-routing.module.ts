a3736daf87093baf05b1de83f39eaadb84c047cf
Ernestekas <ernestekas@gmail.com>
2022-02-01 22:59:40 +0200

    Added some routings.

--- ShopApp-0201/src/app/app-routing.module.ts
+++ ShopApp-0201/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+import { ShopComponent } from './components/Shop/shop/shop.component';
+import { ShopsListComponent } from './components/ShopsList/shops-list/shops-list.component';
+
+const routes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'shops', component: ShopsListComponent},
+  {path: 'shop', component: ShopComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
